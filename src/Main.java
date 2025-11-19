a7a58568c66ef0547317efa5ae2fcfe20db0ca92
alejandro.ticona <alejandro.ticona@et.bo>
2025-11-19 13:30:25 -0400

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Autor: alejandro.ticona
+ * Fecha: 11/19/2025
+ */
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
